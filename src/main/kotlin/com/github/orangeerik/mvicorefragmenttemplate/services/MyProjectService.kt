5b62d19157e694a033979f5b2bd6673cf0c2b09b
GitHub Action <action@github.com>
2023-11-20 19:57:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/orangeerik/mvicorefragmenttemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/orangeerik/mvicorefragmenttemplate/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.orangeerik.mvicorefragmenttemplate.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.orangeerik.mvicorefragmenttemplate.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
